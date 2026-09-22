d8252188738422d66ca51501e85ac1dca7abb5d7
fedeiribarriaa-coder <fedeiribarriaa@gmail.com>
2026-09-21 22:01:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ function setHeroDog(index, initial=false){
       name.textContent=dog.name.toUpperCase();
       meta.textContent=dog.meta;
       card.classList.remove('is-changing');
-    },290);
+    },300);
 
   }else{
     img.src=dog.img;
