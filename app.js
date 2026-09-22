8ff8e0420d732a00222ab11d479751a9626e734f
fedeiribarriaa-coder <fedeiribarriaa@gmail.com>
2026-09-21 21:56:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ function startHeroCarousel(){
   heroTimer=window.setInterval(()=>{
     heroIndex=(heroIndex+1)%heroDogs.length;
     setHeroDog(heroIndex);
-  },400);
+  },260);
 }
 
 startHeroCarousel();
